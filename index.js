7a8cf7b0e923f2d7f1b7037b6b8ed6b1be09ded1
TOMAS CZERNUSZKA SCHIRMER <47569637@ort>
2024-04-25 12:00:30 -0300

    Index

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
 import express from "express"; // npm install express
 import cors from "cors"; // npm install cors
+import ProvinceRouter from "./controllers/province-controller.js"
 
+const app = express();
+const por = 3000;
+
+//Middlewares
+app.use(cors());
+app.use(express.json());
+
+//Endpoints
+
+app.use("/api/province", ProvinceRouter);
+
+//Inicio el server y lo pongo a escuchar
+
+app.listen(port, () => {
+    console.log(`Example app listening on por ${port}`)
+});
