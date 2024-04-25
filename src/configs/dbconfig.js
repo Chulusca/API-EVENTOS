29ea6c534462b15464b3de0b8de118cde9e28c70
TOMAS CZERNUSZKA SCHIRMER <47569637@ort>
2024-04-25 12:11:03 -0300

    Estructura

---
+++ src/configs/dbconfig.js
@@ -0,0 +1,9 @@
+const config = {
+    host: "localhost",
+    database: "dai-Events",
+    user: "root",
+    password: "root",
+    port: 5432
+}
+
+export default config;
---
+++ src/entities/province.js
@@ -0,0 +1,10 @@
+class Province{
+    id;
+    name;
+    full_name;
+    latitudeM
+    longitude;
+    display_order;
+}
+
+export default Province;
