bf0df52926fea2e82af6372a8bfeab9cbaea63ba
TOMAS CZERNUSZKA SCHIRMER <47569637@ort>
2024-06-05 09:44:51 -0300

    BD REMOTE HOST

--- src/configs/dbconfig.js
+++ src/configs/dbconfig.js
@@ -1,11 +1,14 @@
 import 'dotenv/config'
 
 const config = {
-    host: process.env.DB_HOST ?? '',
+    host: process.env.DB_REMOTE_HOST ?? '',
     database: process.env.DB_DATABASE ?? '',
     user: process.env.DB_USER ?? '',
     password: process.env.DB_PASSWORD ?? '',
-    port: process.env.DB_PORT ?? 5432
+    port: process.env.DB_PORT ?? 5432,
+    ssl:{
+        rejectUnauthorized: false
+    }
 }
 
 export default config;
